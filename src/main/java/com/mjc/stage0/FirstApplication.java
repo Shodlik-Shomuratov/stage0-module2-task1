eeecf7ed0bcfa6016867385bb0668dfb7701dd33
Shodlik Shomuratov <shodlikcoder@gmail.com>
2022-11-24 18:29:13 +0500

    Update FirstApplication.java file comment.

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,7 +2,7 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+        // I wrote code here
         System.out.println("Hello, World!");
     }
 }
